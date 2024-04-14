5f80e2de329d5b29706c5154846d2bf235e48bc0
rokun@jekk.com <Tafsap-xuzpy7-wypvuz>
2024-04-14 12:50:16 +0600

    Updated routing code

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './component/HomePage';
 import SubPage1 from './component/SubPage1';
 import SubPage2 from './component/SubPage2';
+import NotFound from './component/NotFound'; // Import the NotFound component
 
 function App() {
-  return (
-      <Router basename="/pov">
-          <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/subpage1" element={<SubPage1 />} />
-              <Route path="/subpage2" element={<SubPage2 />} />
-              <Route path="*" element={<NotFound />} />
-          </Routes>
-      </Router>
-  );
+    return (
+        <Router basename="/pov">
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/subpage1" element={<SubPage1 />} />
+                <Route path="/subpage2" element={<SubPage2 />} />
+                <Route path="*" element={<NotFound />} /> // Use the NotFound component
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
