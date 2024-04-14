eb73ed85ec02813508c55ce2674d7ab43a560d7a
rokun@jekk.com <Tafsap-xuzpy7-wypvuz>
2024-04-14 12:41:07 +0600

    Updated routing code

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ import SubPage1 from './component/SubPage1';
 import SubPage2 from './component/SubPage2';
 
 function App() {
-    return (
-        <Router basename="/pov">
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/subpage1" element={<SubPage1 />} />
-                <Route path="/subpage2" element={<SubPage2 />} />
-                {/* Catch-all route for any undefined paths */}
-                <Route path="*" element={<HomePage />} />
-            </Routes>
-        </Router>
-    );
+  return (
+      <Router basename="/pov">
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/subpage1" element={<SubPage1 />} />
+              <Route path="/subpage2" element={<SubPage2 />} />
+              <Route path="*" element={<NotFound />} />
+          </Routes>
+      </Router>
+  );
 }
 
 export default App;
