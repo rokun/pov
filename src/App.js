57c19af57bd36d8276f35297325dadb933a405fd
rokun@jekk.com <Tafsap-xuzpy7-wypvuz>
2024-04-14 12:11:29 +0600

    Message

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import SubPage1 from './component/SubPage1';
 import SubPage2 from './component/SubPage2';
 
 function App() {
-  return (
-    
-    <Router >
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/subpage1" element={<SubPage1 />} />
-        <Route path="/subpage2" element={<SubPage2 />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router basename="/pov">
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/subpage1" element={<SubPage1 />} />
+                <Route path="/subpage2" element={<SubPage2 />} />
+                {/* Catch-all route for any undefined paths */}
+                <Route path="*" element={<HomePage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
